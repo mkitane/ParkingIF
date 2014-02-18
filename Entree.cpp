17084a9b9accb2ec7a132f7825f3f042c46b514b
Kitane Mehdi <mehdikitane@gmail.com>
2014-02-18 16:37:08 +0100

    Ajout Entree

--- Entree.cpp
+++ Entree.cpp
@@ -1,27 +1,22 @@
 /*************************************************************************
-                           Mere  -  description
+                           Entree  -  description
                              -------------------
-    début                : Mere
-    copyright            : (C) Mere par Mere
-    e-mail               : Mere
+    début                : Entree
+    copyright            : (C) Entree par Entree
+    e-mail               : Entree
 *************************************************************************/
 
-//---------- Réalisation de la tâche <Mere> (fichier Mere.cpp) ---
+//---------- Réalisation de la tâche <Entree> (fichier Entree.cpp) ---
 
 /////////////////////////////////////////////////////////////////  INCLUDE
 //-------------------------------------------------------- Include système
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <signal.h>
+
 //------------------------------------------------------ Include personnel
-#include "Mere.h"
-#include "Clavier.h"
-#include "/public/tp/tp-multitache/Outils.h"
-#include "/public/tp/tp-multitache/Heure.h"
+#include "Entree.h"
+
 ///////////////////////////////////////////////////////////////////  PRIVE
 //------------------------------------------------------------- Constantes
-#define TERMINALUTILISE XTERM
+
 //------------------------------------------------------------------ Types
 
 //---------------------------------------------------- Variables statiques
@@ -39,34 +34,8 @@
 
 //////////////////////////////////////////////////////////////////  PUBLIC
 //---------------------------------------------------- Fonctions publiques
-static pid_t noClavier;
-static pid_t noHeure;
-static pid_t noEntreeUn;
-static pid_t noEntreeDeux;
-static pid_t noEntreeTrois;
-int main (void)
-//Algorithme :
+void Entree(int Parametrage)
 {
-	InitialiserApplication(TERMINALUTILISE);
 
-	if( (noClavier = fork()) == 0)
-	{
-		/*Code du fils */
-		Clavier();
-	}else{
-		/*Code du pere */
-		noHeure = ActiverHeure();
-
-		waitpid(noClavier, NULL, 0); //On attend la reception de Q du clavier
-
-		//Fin de tache heure apres reception de SIGUSR2
-		kill(noHeure, SIGUSR2);
-
-
-		TerminerApplication();
-		exit(0);
-
-	}
-
-	return 0;
+}
 }
